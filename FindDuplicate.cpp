55c29b4c82a54214e51717a9035db5b9580bf85f
Abhisekh Nayek <115914826+AbhisekhNayek@users.noreply.github.com>
2023-11-17 18:00:30 +0530

    Create FindDuplicate.cpp

---
+++ FindDuplicate.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int findDuplicate(vector < int > & arr) {
+  int n = arr.size();
+  int freq[n + 1] = {
+    0
+  };
+  for (int i = 0; i < n; i++) {
+    if (freq[arr[i]] == 0) {
+      freq[arr[i]] += 1;
+    } else {
+      return arr[i];
+    }
+  }
+  return 0;
+}
+int main() {
+  vector < int > arr;
+  arr = {1,3,4,2,3};
+  cout << "The duplicate element is " << findDuplicate(arr) << endl;
+}
