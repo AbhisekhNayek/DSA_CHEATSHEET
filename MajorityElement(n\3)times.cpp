ba4eb0e831ae0ee0f731bfc1a766951e1b5ca46f
Abhisekh Nayek <115914826+AbhisekhNayek@users.noreply.github.com>
2023-11-20 12:06:54 +0530

    Create MajorityElement(n\3)times.cpp

---
+++ MajorityElement(n\3)times.cpp
@@ -0,0 +1,61 @@
+
+
+
+int #include <bits/stdc++.h>
+using namespace std;
+
+vector<int> majorityElement(vector<int> v) {
+    int n = v.size(); //size of the array
+
+    int cnt1 = 0, cnt2 = 0; // counts
+    int el1 = INT_MIN; // element 1
+    int el2 = INT_MIN; // element 2
+
+    // applying the Extended Boyer Moore's Voting Algorithm:
+    for (int i = 0; i < n; i++) {
+        if (cnt1 == 0 && el2 != v[i]) {
+            cnt1 = 1;
+            el1 = v[i];
+        }
+        else if (cnt2 == 0 && el1 != v[i]) {
+            cnt2 = 1;
+            el2 = v[i];
+        }
+        else if (v[i] == el1) cnt1++;
+        else if (v[i] == el2) cnt2++;
+        else {
+            cnt1--, cnt2--;
+        }
+    }
+
+    vector<int> ls; // list of answers
+
+    // Manually check if the stored elements in
+    // el1 and el2 are the majority elements:
+    cnt1 = 0, cnt2 = 0;
+    for (int i = 0; i < n; i++) {
+        if (v[i] == el1) cnt1++;
+        if (v[i] == el2) cnt2++;
+    }
+
+    int mini = int(n / 3) + 1;
+    if (cnt1 >= mini) ls.push_back(el1);
+    if (cnt2 >= mini) ls.push_back(el2);
+
+    // Uncomment the following line
+    // if it is told to sort the answer array:
+    // sort(ls.begin(), ls.end()); //TC --> O(2*log2) ~ O(1);
+
+    return ls;
+}
+
+int main()
+{
+    vector<int> arr = {11, 33, 33, 11, 33, 11};
+    vector<int> ans = majorityElement(arr);
+    cout << "The majority elements are: ";
+    for (auto it : ans)
+        cout << it << " ";
+    cout << "\n";
+    return 0;
+}
