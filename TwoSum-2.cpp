12f36dfcb4bd56cd004b6b08022d80bf737c3023
Abhisekh Nayek <115914826+AbhisekhNayek@users.noreply.github.com>
2023-11-22 11:04:40 +0530

    Create TwoSum-2.cpp

---
+++ TwoSum-2.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> twoSum(int n, vector<int> &arr, int target) {
+    unordered_map<int, int> mpp;
+    for (int i = 0; i < n; i++) {
+        int num = arr[i];
+        int moreNeeded = target - num;
+        if (mpp.find(moreNeeded) != mpp.end()) {
+            return {mpp[moreNeeded], i};
+        }
+        mpp[num] = i;
+    }
+    return { -1, -1};
+}
+
+int main()
+{
+    int n = 5;
+    vector<int> arr = {2, 6, 5, 8, 11};
+    int target = 14;
+    vector<int> ans = twoSum(n, arr, target);
+    cout << "This is the answer for variant 2: [" << ans[0] << ", "
+         << ans[1] << "]" << endl;
+    return 0;
+}
