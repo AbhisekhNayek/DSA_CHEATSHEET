8dbf5a1542d800ad8e5827b1cd3481d3c467fd93
Abhisekh Nayek <115914826+AbhisekhNayek@users.noreply.github.com>
2023-11-23 22:24:55 +0530

    Create LongestConscutiveSequence.cpp

---
+++ LongestConscutiveSequence.cpp
@@ -0,0 +1,42 @@
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int longestSuccessiveElements(vector<int>&a) {
+    int n = a.size();
+    if (n == 0) return 0;
+
+    int longest = 1;
+    unordered_set<int> st;
+    //put all the array elements into set:
+    for (int i = 0; i < n; i++) {
+        st.insert(a[i]);
+    }
+
+    //Find the longest sequence:
+    for (auto it : st) {
+        //if 'it' is a starting number:
+        if (st.find(it - 1) == st.end()) {
+            //find consecutive numbers:
+            int cnt = 1;
+            int x = it;
+            while (st.find(x + 1) != st.end()) {
+                x = x + 1;
+                cnt = cnt + 1;
+            }
+            longest = max(longest, cnt);
+        }
+    }
+    return longest;
+
+}
+
+int main()
+{
+    vector<int> a = {100, 200, 1, 2, 3, 4};
+    int ans = longestSuccessiveElements(a);
+    cout << "The longest consecutive sequence is " << ans << "\n";
+    return 0;
+}
