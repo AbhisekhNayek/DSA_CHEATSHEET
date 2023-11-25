262cc94f49d9365422def251857eac600c043317
Abhisekh Nayek <115914826+AbhisekhNayek@users.noreply.github.com>
2023-11-25 16:14:29 +0530

    Create CountNumberOfSubarraysWithGiven_XoR_K.cpp

---
+++ CountNumberOfSubarraysWithGiven_XoR_K.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int subarraysWithXorK(vector<int> a, int k) {
+    int n = a.size(); //size of the given array.
+    int xr = 0;
+    map<int, int> mpp; //declaring the map.
+    mpp[xr]++; //setting the value of 0.
+    int cnt = 0;
+
+    for (int i = 0; i < n; i++) {
+        // prefix XOR till index i:
+        xr = xr ^ a[i];
+
+        //By formula: x = xr^k:
+        int x = xr ^ k;
+
+        // add the occurrence of xr^k
+        // to the count:
+        cnt += mpp[x];
+
+        // Insert the prefix xor till index i
+        // into the map:
+        mpp[xr]++;
+    }
+    return cnt;
+}
+
+int main()
+{
+    vector<int> a = {4, 2, 2, 6, 4};
+    int k = 6;
+    int ans = subarraysWithXorK(a, k);
+    cout << "The number of subarrays with XOR k is: "
+         << ans << "\n";
+    return 0;
+}
