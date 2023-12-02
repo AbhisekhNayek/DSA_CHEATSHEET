9a955f245fc2c229b85e21e3add6eec526418b99
Abhisekh Nayek <115914826+AbhisekhNayek@users.noreply.github.com>
2023-12-02 17:25:20 +0530

    Create RotateLinkedList.cpp

---
+++ RotateLinkedList.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class node {
+    public:
+        int num;
+        node* next;
+        node(int a) {
+            num = a;
+            next = NULL;
+        }
+};
+//utility function to insert node at the end of the list
+void insertNode(node* &head,int val) {
+    node* newNode = new node(val);
+    if(head == NULL) {
+        head = newNode;
+        return;
+    }
+    node* temp = head;
+    while(temp->next != NULL) temp = temp->next;
+    
+    temp->next = newNode;
+    return;
+}
+//utility function to rotate list by k times
+node* rotateRight(node* head,int k) {
+    if(head == NULL||head->next == NULL||k == 0) return head;
+    //calculating length
+    node* temp = head;
+    int length = 1;
+    while(temp->next != NULL) {
+        ++length;
+        temp = temp->next;
+    }
+    //link last node to first node
+    temp->next = head;
+    k = k%length; //when k is more than length of list
+    int end = length-k; //to get end of the list
+    while(end--) temp = temp->next;
+    //breaking last node link and pointing to NULL
+    head = temp->next;
+    temp->next = NULL;
+        
+    return head;
+}
+
+//utility function to print list
+void printList(node* head) {
+    while(head->next != NULL) {
+        cout<<head->num<<"->";
+        head = head->next;
+    } 
+    cout<<head->num<<endl;
+    return;
+}
+
+int main() {
+    node* head = NULL;
+    //inserting Node
+    insertNode(head,1);
+    insertNode(head,2);
+    insertNode(head,3);
+    insertNode(head,4);
+    insertNode(head,5);
+    
+    cout<<"Original list: ";
+    printList(head);
+    
+    int k = 2;
+    node* newHead = rotateRight(head,k);//calling function for rotating right of the nodes by k times
+    
+    cout<<"After "<<k<<" iterations: ";
+    printList(newHead);//list after rotating nodes
+    return 0;
+}
