c8c68e3b9cf1ea077a2acb165cde8981c401233f
Abhisekh Nayek <115914826+AbhisekhNayek@users.noreply.github.com>
2023-12-11 09:38:15 +0530

    Create PrintAllPermutationOfAString.cpp

---
+++ PrintAllPermutationOfAString.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+class Solution {
+  private:
+    void recurPermute(int index, vector < int > & nums, vector < vector < int >> & ans) {
+      if (index == nums.size()) {
+        ans.push_back(nums);
+        return;
+      }
+      for (int i = index; i < nums.size(); i++) {
+        swap(nums[index], nums[i]);
+        recurPermute(index + 1, nums, ans);
+        swap(nums[index], nums[i]);
+      }
+    }
+  public:
+    vector < vector < int >> permute(vector < int > & nums) {
+      vector < vector < int >> ans;
+      recurPermute(0, nums, ans);
+      return ans;
+    }
+};
+
+int main() {
+  Solution obj;
+  vector < int > v {1,2,3};
+  vector < vector < int >> sum = obj.permute(v);
+  cout << "All Permutations are" << endl;
+  for (int i = 0; i < sum.size(); i++) {
+    for (int j = 0; j < sum[i].size(); j++)
+      cout << sum[i][j] << " ";
+    cout << endl;
+  }
+}
