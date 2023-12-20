121536abe4f813de8f2d88afe54e733717cea2fc
Abhisekh Nayek <115914826+AbhisekhNayek@users.noreply.github.com>
2023-12-20 09:30:24 +0530

    Create QueueUsingSingleStack.cpp

---
+++ QueueUsingSingleStack.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class MyQueue {
+  public:
+    stack < int > input, output;
+  /** Initialize your data structure here. */
+  MyQueue() {
+
+  }
+
+  /** Push element x to the back of queue. */
+  void push(int x) {
+    cout << "The element pushed is " << x << endl;
+    input.push(x);
+  }
+
+  /** Removes the element from in front of queue and returns that element. */
+  int pop() {
+    // shift input to output 
+    if (output.empty())
+      while (input.size())
+        output.push(input.top()), input.pop();
+
+    int x = output.top();
+    output.pop();
+    return x;
+  }
+
+  /** Get the front element. */
+  int top() {
+    // shift input to output 
+    if (output.empty())
+      while (input.size())
+        output.push(input.top()), input.pop();
+    return output.top();
+  }
+
+  int size() {
+    return (output.size() + input.size()); 
+  }
+
+};
+int main() {
+  MyQueue q;
+  q.push(3);
+  q.push(4);
+  cout << "The element poped is " << q.pop() << endl;
+  q.push(5);
+  cout << "The top of the queue is " << q.top() << endl;
+  cout << "The size of the queue is " << q.size() << endl;
+
+}
